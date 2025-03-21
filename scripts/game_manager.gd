fb8028a07960b4f14c8eb6f9a26f09ac152433be
NASSER <massoudibarak@gmail.com>
2025-03-21 23:41:03 +0100

    mini changes

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -1,9 +1,10 @@
 extends Node
 
+@onready var points_label: Label = %"points label"
 
 var points = 0
 
 func add_point():
 	points += 1
 	print(points)
-	
+	points_label.text = "points: " + str(points)
