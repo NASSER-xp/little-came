1da5b1e48c4d9faa93f72415da4123e52376308a
NASSER <massoudibarak@gmail.com>
2025-03-21 16:58:54 +0100

    some fixes
    -fix the collectable coin

--- coin_collectable.gd
+++ coin_collectable.gd
@@ -1,1 +1,8 @@
 extends Area2D
+
+@onready var game_manager: Node = %gameManager
+
+func _on_body_entered(body: Node2D) -> void:
+	if (body.name == "CharacterBody2D"):
+		queue_free() # Replace with function body.
+		game_manager.add_point()
