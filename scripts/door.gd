7b667fd96e23a5b807f1f1ef35a2272188306ce9
A7-R2 <abdenourbrraki@gmail.com>
2026-03-26 19:23:10 +0100

    Fixed door proximity2

--- scripts/door.gd
+++ scripts/door.gd
@@ -1,16 +1,12 @@
 extends Area2D
 
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+var is_open := false
 
 func _ready() -> void:
 	body_entered.connect(_on_body_entered)
-	body_exited.connect(_on_body_exited)
 
 func _on_body_entered(body: Node2D) -> void:
-	if body is CharacterBody2D and not animated_sprite.is_playing():
+	if body is CharacterBody2D and not is_open:
+		is_open = true
 		animated_sprite.play("default")
-
-func _on_body_exited(body: Node2D) -> void:
-	if body is CharacterBody2D:
-		animated_sprite.stop()
-		animated_sprite.frame = 0
