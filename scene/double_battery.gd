21953454e6585326112346a6ddc2dcdebeb033ef
A7-R2 <abdenourbrraki@gmail.com>
2026-03-27 00:29:01 +0100

    Batteries fixes

--- scene/double_battery.gd
+++ scene/double_battery.gd
@@ -1,10 +1,13 @@
 extends Area2D
 
 @onready var game_manager: Node = %gameManager
-@export var energy_gain: float = 150.0
+@export var energy_gain: float = 200.0
+@export var max_energy_bonus: float = 200.0
 
 func _on_body_entered(body: Node2D) -> void:
 	if (body.name == "CharacterBody2D"):
+		if max_energy_bonus > 0.0 and body.has_method("increase_max_energy"):
+			body.increase_max_energy(max_energy_bonus)
 		if body.has_method("add_energy"):
 			body.add_energy(energy_gain)
 		queue_free() # حذف العملة بعد الجمع
