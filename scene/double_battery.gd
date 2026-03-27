8ebd6bbfeb184b37f3d20be834eff16e35101f4b
NASSER <massoudibarak@gmail.com>
2026-03-27 11:19:19 +0100

    lvl3-2

--- scene/double_battery.gd
+++ scene/double_battery.gd
@@ -1,8 +1,8 @@
 extends Area2D
 
 @onready var game_manager: Node = %gameManager
-@export var energy_gain: float = 200.0
-@export var max_energy_bonus: float = 200.0
+@export var energy_gain: float = 500.0
+@export var max_energy_bonus: float = 500.0
 
 func _on_body_entered(body: Node2D) -> void:
 	if (body.name == "CharacterBody2D"):
@@ -11,4 +11,3 @@ func _on_body_entered(body: Node2D) -> void:
 		if body.has_method("add_energy"):
 			body.add_energy(energy_gain)
 		queue_free() # حذف العملة بعد الجمع
-		
